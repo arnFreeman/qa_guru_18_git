eb1d96a538176eeda65463245ed6004af5a1f083
Renat Abdullin <158848533+arnFreeman@users.noreply.github.com>
2024-02-06 01:36:30 +0500

    Update Main.java

--- src/main/java/ru/freeman/Main.java
+++ src/main/java/ru/freeman/Main.java
@@ -1,7 +1,7 @@
 package ru.freeman;
 
 public class Main {
-private static String msg = ""Hello qa.guru!!!!!""
+private static String msg = "Hello qa.guru!";
     public static void main(String[] args) {
 
         System.out.println(msg);
